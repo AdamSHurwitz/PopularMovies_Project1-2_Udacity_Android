6472b1cd01adefa3b1b55be277ea8e9c4a1b40d6
adamhurwitz <adamscotthurwitz@gmail.com>
2016-01-29 17:05:13 -0800

    Built ContentProvider

--- app/src/main/java/com/adamhurwitz/android/popularmovies/data/CursorContract.java
+++ app/src/main/java/com/adamhurwitz/android/popularmovies/data/CursorContract.java
@@ -1,5 +1,8 @@
 package com.adamhurwitz.android.popularmovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,11 +13,38 @@ public class CursorContract {
     // give it an empty constructor.
     public CursorContract(){}
 
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.adamhurwitz.android.popularmovies";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
+    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
+    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
+    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+    public static final String PATH_MOVIETABLE = "movie_table";
+
     /*
         Inner class that defines the table contents of the table
      */
     public static abstract class MovieData implements BaseColumns {
-        public static final String TABLE_NAME = "movie_data";
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_MOVIETABLE).build();
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/"
+                        + PATH_MOVIETABLE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/"
+                        + PATH_MOVIETABLE;
+
+        public static final String TABLE_NAME = "movie_table";
         public static final String COLUMN_NAME_MOVIEID = "movie_id";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_IMAGEURL = "image_url";
@@ -27,6 +57,10 @@ public class CursorContract {
         public static final String COLUMN_NAME_REVIEW_1 = "review_1";
         public static final String COLUMN_NAME_REVIEW_2 = "review_2";
         public static final String COLUMN_NAME_REVIEW_3 = "review_3";
+
+        public static Uri buildProductUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
 }
---
+++ app/src/main/java/com/adamhurwitz/android/popularmovies/data/MovieProvider.java
@@ -0,0 +1,205 @@
+package com.adamhurwitz.android.popularmovies.data;
+
+import android.annotation.TargetApi;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by adamhurwitz on 1/29/16.
+ */
+public class MovieProvider extends ContentProvider{
+    private static final String LOG_TAG = MovieProvider.class.getSimpleName();
+
+    // The URI Matcher used by this content provider.
+    private static final UriMatcher uriMatcher = buildUriMatcher();
+    private CursorDbHelper cursorDbHelper;
+    // Defines the database name
+    private static final String DBNAME = "movie_data.db";
+    static final int MOVIE = 85;
+    Context context;
+
+
+    @Override
+    public boolean onCreate() {
+        cursorDbHelper = new CursorDbHelper(getContext());
+        return true;
+    }
+
+    /*
+       Students: Here is where you need to create the UriMatcher. This UriMatcher will
+       match each URI to the WEATHER, WEATHER_WITH_LOCATION, WEATHER_WITH_LOCATION_AND_DATE,
+       and LOCATION integer constants defined above.  You can test this by uncommenting the
+       testUriMatcher test within TestUriMatcher.
+    */
+    static UriMatcher buildUriMatcher() {
+        // All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found.  The code passed into the constructor represents the code to return for the root
+        // URI.  It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = CursorContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding URI.
+        matcher.addURI(authority, CursorContract.PATH_MOVIETABLE, MOVIE);
+        // use 'ContentProviderContract.PATH_PRODUCT + "/*"' for path with one additional item
+        // string or 'ContentProviderContract.PATH_PRODUCT + "/*/#"' for path with string then a num
+        Log.v(LOG_TAG, "UriMatcher_returns :" + String.valueOf(matcher));
+        return matcher;
+    }
+
+
+    // getType() not important since we're not working with BLOB data type
+    @Override
+    public String getType(Uri uri) {
+
+        // Use the Uri Matcher to determine what kind of URI this is.
+        final int match = uriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIE:
+                Log.v(LOG_TAG, "getType :" + CursorContract.MovieData.CONTENT_TYPE);
+                return CursorContract.MovieData.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Here's the switch statement that, given a URI, will determine what kind of request it is,
+        // and query the database accordingly.
+        Cursor cursor;
+        switch (uriMatcher.match(uri)) {
+            case MOVIE: {
+                //retCursor = productCursor(uri, projection, sortOrder);
+                cursor = cursorDbHelper.getReadableDatabase().query(
+                        CursorContract.MovieData.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = cursorDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        Uri returnUri;
+        switch (match) {
+            case MOVIE: {
+                long _id = db.insert(CursorContract.MovieData.TABLE_NAME,
+                        null, values);
+                if (_id > 0)
+                    returnUri = CursorContract.MovieData.buildProductUri(
+                            _id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int update(
+            Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = cursorDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case MOVIE:
+                rowsUpdated = db.update(CursorContract.MovieData
+                                .TABLE_NAME, values,
+                        selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = cursorDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        int rowsDeleted;
+        // this makes delete all rows return the number of rows deleted
+        if (null == selection) selection = "1";
+        switch (match) {
+            case MOVIE:
+                rowsDeleted = db.delete(
+                        CursorContract.MovieData.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // Because a null deletes all rows
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = cursorDbHelper.getWritableDatabase();
+        final int match = uriMatcher.match(uri);
+        switch (match) {
+            case MOVIE:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(CursorContract.MovieData
+                                .TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
+    // You do not need to call this method. This is a method specifically to assist the testing
+    // framework in running smoothly. You can read more at:
+    // http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        cursorDbHelper.close();
+        super.shutdown();
+    }
+}
