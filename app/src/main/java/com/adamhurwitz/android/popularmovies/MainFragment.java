7a3afb30768bd2553ed1c469ddafdac5dd7ecb3c
adamhurwitz <adamscotthurwitz@gmail.com>
2016-01-31 02:18:31 -0800

    Change UI based on Settings - MainFragment.java

--- app/src/main/java/com/adamhurwitz/android/popularmovies/MainFragment.java
+++ app/src/main/java/com/adamhurwitz/android/popularmovies/MainFragment.java
@@ -45,6 +45,77 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Call AsyncTask to get Movie Data
+        /*SharedPreferences sql_pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String sort_value = sql_pref.getString("sort_key", "popularity.desc");
+        Log.v(LOG_TAG, "CALLED_ON_START | " + sort_value);*/
+        getMovieData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.grid_view_layout, container, false);
+
+      /*  SharedPreferences sql_pref = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String sort_value = sql_pref.getString("sort_key", "popularity.desc");
+        Log.v(LOG_TAG,"CALLED_ON_ONCREATEVIEW | "+sort_value);
+        initialPref = sort_value;*/
+
+        // Create menu
+        setHasOptionsMenu(true);
+
+        // Initialize Adapter
+        mAsyncCursorAdapter = new com.adamhurwitz.android.popularmovies.AsyncCursorAdapter(
+                getActivity(), null, 0);
+        Log.v("CursorAdapter_Called", "HERE");
+
+        // Get a reference to the grid view layout and attach the adapter to it.
+        GridView gridView = (GridView) view.findViewById(R.id.grid_view_layout);
+        gridView.setAdapter(mAsyncCursorAdapter);
+
+        // Click listener when grid item is selected
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+                String movie_id = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_MOVIEID));
+                String title = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_TITLE));
+                String image_url = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_IMAGEURL));
+                String summary = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_SUMMARY));
+                String rating = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_VOTEAVERAGE));
+                String popularity = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_POPULARITY));
+                String release_date = cursor.getString(cursor.getColumnIndex(CursorContract
+                        .MovieData
+                        .COLUMN_NAME_RELEASEDATE));
+                String favorite = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
+                        .COLUMN_NAME_FAVORITE));
+
+                // store items to pass into intent
+                String[] doodleDataItems = {movie_id, title, image_url, summary, rating,
+                        release_date, favorite};
+                Intent intent = new Intent(getActivity(),
+                        com.adamhurwitz.android.popularmovies.DetailActivity.class);
+                intent.putExtra("Cursor Movie Attributes", doodleDataItems);
+                startActivity(intent);
+
+                // launch method that executes AsyncTask to build YouTube URL and update database
+                getYouTubeKey(movie_id, title);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         // Check initial status of SharedPreference value against current
@@ -110,79 +181,6 @@ public class MainFragment extends Fragment implements LoaderManager.LoaderCallba
             mAsyncCursorAdapter.changeCursor(onResumeCursor);
         }
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Call AsyncTask to get Movie Data
-        /*SharedPreferences sql_pref = PreferenceManager.getDefaultSharedPreferences(getContext());
-        String sort_value = sql_pref.getString("sort_key", "popularity.desc");
-        Log.v(LOG_TAG, "CALLED_ON_START | " + sort_value);*/
-        getMovieData();
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.grid_view_layout, container, false);
-
-      /*  SharedPreferences sql_pref = PreferenceManager.getDefaultSharedPreferences(getContext());
-        String sort_value = sql_pref.getString("sort_key", "popularity.desc");
-        Log.v(LOG_TAG,"CALLED_ON_ONCREATEVIEW | "+sort_value);
-        initialPref = sort_value;*/
-
-        // Create menu
-        setHasOptionsMenu(true);
-
-        // Initialize Adapter
-        mAsyncCursorAdapter = new com.adamhurwitz.android.popularmovies.AsyncCursorAdapter(
-                getActivity(), null, 0);
-        Log.v("CursorAdapter_Called", "HERE");
-
-        // Get a reference to the grid view layout and attach the adapter to it.
-        GridView gridView = (GridView) view.findViewById(R.id.grid_view_layout);
-        gridView.setAdapter(mAsyncCursorAdapter);
-
-        // Click listener when grid item is selected
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
-                String movie_id = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_MOVIEID));
-                String title = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_TITLE));
-                String image_url = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_IMAGEURL));
-                String summary = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_SUMMARY));
-                String rating = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_VOTEAVERAGE));
-                String popularity = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_POPULARITY));
-                String release_date = cursor.getString(cursor.getColumnIndex(CursorContract
-                        .MovieData
-                        .COLUMN_NAME_RELEASEDATE));
-                String favorite = cursor.getString(cursor.getColumnIndex(CursorContract.MovieData
-                        .COLUMN_NAME_FAVORITE));
-
-                // store items to pass into intent
-                String[] doodleDataItems = {movie_id, title, image_url, summary, rating,
-                        release_date, favorite};
-                Intent intent = new Intent(getActivity(),
-                        com.adamhurwitz.android.popularmovies.DetailActivity.class);
-                intent.putExtra("Cursor Movie Attributes", doodleDataItems);
-                startActivity(intent);
-
-                // launch method that executes AsyncTask to build YouTube URL and update database
-                getYouTubeKey(movie_id, title);
-            }
-        });
-
-        return view;
-    }
-
 
     // Create menu items and actions
     @Override
